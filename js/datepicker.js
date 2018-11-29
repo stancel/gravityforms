caae069782a2048f9d74c2f286a593cf15f09955
Brad Stancel <brad.stancel@gmail.com>
2018-11-29 12:27:35 -0500

    Updating to version 2.4.0.1

--- js/datepicker.js
+++ js/datepicker.js
@@ -25,38 +25,38 @@ function gformInitDatepicker() {
                 }
             };
 
-        if ($element.hasClass('dmy')) {
-            optionsObj.dateFormat = 'dd/mm/yy';
-        } else if ($element.hasClass('dmy_dash')) {
-            optionsObj.dateFormat = 'dd-mm-yy';
-        } else if ($element.hasClass('dmy_dot')) {
-            optionsObj.dateFormat = 'dd.mm.yy';
-        } else if ($element.hasClass('ymd_slash')) {
-            optionsObj.dateFormat = 'yy/mm/dd';
-        } else if ($element.hasClass('ymd_dash')) {
-            optionsObj.dateFormat = 'yy-mm-dd';
-        } else if ($element.hasClass('ymd_dot')) {
-            optionsObj.dateFormat = 'yy.mm.dd';
-        }
+	    if ($element.hasClass('dmy')) {
+		    optionsObj.dateFormat = 'dd/mm/yy';
+	    } else if ($element.hasClass('dmy_dash')) {
+		    optionsObj.dateFormat = 'dd-mm-yy';
+	    } else if ($element.hasClass('dmy_dot')) {
+		    optionsObj.dateFormat = 'dd.mm.yy';
+	    } else if ($element.hasClass('ymd_slash')) {
+		    optionsObj.dateFormat = 'yy/mm/dd';
+	    } else if ($element.hasClass('ymd_dash')) {
+		    optionsObj.dateFormat = 'yy-mm-dd';
+	    } else if ($element.hasClass('ymd_dot')) {
+		    optionsObj.dateFormat = 'yy.mm.dd';
+	    }
 
-        if ($element.hasClass('datepicker_with_icon')) {
-            optionsObj.showOn = 'both';
-            optionsObj.buttonImage = jQuery('#gforms_calendar_icon_' + inputId).val();
-            optionsObj.buttonImageOnly = true;
-        }
+	    if ($element.hasClass('datepicker_with_icon')) {
+		    optionsObj.showOn = 'both';
+		    optionsObj.buttonImage = jQuery(this).parent().siblings("[id^='gforms_calendar_icon_input']").val();
+		    optionsObj.buttonImageOnly = true;
+	    }
 
         inputId = inputId.split('_');
 
-        // allow the user to override the datepicker options object
-        optionsObj = gform.applyFilters('gform_datepicker_options_pre_init', optionsObj, inputId[1], inputId[2]);
+	    // allow the user to override the datepicker options object
+	    optionsObj = gform.applyFilters('gform_datepicker_options_pre_init', optionsObj, inputId[1], inputId[2]);
 
-        $element.datepicker(optionsObj);
+	    $element.datepicker(optionsObj);
 
-        // We give the input focus after selecting a date which differs from default Datepicker behavior; this prevents
+	    // We give the input focus after selecting a date which differs from default Datepicker behavior; this prevents
 	    // users from clicking on the input again to open the datepicker. Let's add a manual click event to handle this.
-        $element.click( function() {
-        	$element.datepicker( 'show' );
-        } );
+	    $element.click(function () {
+		    $element.datepicker('show');
+	    });
 
     });
 }
